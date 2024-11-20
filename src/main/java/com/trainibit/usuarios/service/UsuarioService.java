cc40dea955cade04c53a1aaa22faed8c30fd41b2
EzaelTorr <torresezael888@gmail.com>
2024-11-20 13:14:51 -0600

    Update UsuarioService.java

--- src/main/java/com/trainibit/usuarios/service/UsuarioService.java
+++ src/main/java/com/trainibit/usuarios/service/UsuarioService.java
@@ -3,7 +3,7 @@ package com.trainibit.usuarios.service;
 import com.trainibit.usuarios.entity.Usuario;
 
 import java.util.List;
-
+//ejemplo
 public interface UsuarioService {
     List<Usuario> findAll();
 }
